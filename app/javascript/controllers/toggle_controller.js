eaf6264a04b1134a7a65e059e137a2155b52ea39
Joe Sak <joe@joesak.com>
2025-09-18 00:06:37 -0600

    Update styles and make UI easier for nested fields

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,16 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = [ "switch", "content" ]
+  static targets = [ "switch", "content", "icon" ]
 
   connect() {
-    this.contentTarget.classList.add('hidden')
+    // this.contentTarget.classList.add('hidden')
+    if (this.contentTarget.classList.contains('hidden'))
+      this.iconTarget.innerHTML = "&plus;"
+    else
+      this.iconTarget.innerHTML = "&minus;"
   }
 
   toggle() {
-    if (this.contentTarget.classList.contains('hidden'))
+    if (this.contentTarget.classList.contains('hidden')) {
+      this.iconTarget.innerHTML = "&minus;"
       this.contentTarget.classList.remove('hidden')
-    else
+    } else {
+      this.iconTarget.innerHTML = "&plus;"
       this.contentTarget.classList.add('hidden')
+    }
   }
 }
